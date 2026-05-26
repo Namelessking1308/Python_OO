fc6eeb087e93d76d047374b1fbd9dd8f86370bcf
Namelessking1308 <ibrahimfakir485@gmail.com>
2026-05-26 11:57:52 +0200

    Ajout de l'exo 4 avec les constructeurs __init__

---
+++ exo4/models/__init__.py
@@ -0,0 +1,13 @@
+from .elephant import Elephant
+from .soigneur import Soigneur
+from .enclos import Enclos
+from .animal import Animal
+from .girafe import Girafe
+
+__all__ = [
+	"Elephant",
+	"Soigneur",
+	"Enclos",
+	"Girafe",
+	"Animal"
+]
